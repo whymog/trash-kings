76318bae599ab1be9a34388496889f5d5d3c129b
Nick Cummings <nickmcummings@gmail.com>
2016-08-30 19:31:20 -0700

    Created build task for gulp; fixed some other stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,40 @@
+"use strict";
+
 var gulp = require('gulp'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del');
 
 gulp.task('concatScripts', function() {
     return gulp.src([
-        'js/*.js',
+        'src/js/allocate.js',
+        'src/js/breeding.js',
+        'src/js/getters.js',
+        'src/js/global-variables.js',
+        'src/js/helpers.js',
+        'src/js/messages.js',
+        'src/js/progress-bar.js',
+        'src/js/raccoons.js',
+        'src/js/game.js'
         ])
+   	.pipe(maps.init())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('js'));
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('src/js'));
 });
 
-gulp.task('default', function() {
-	console.log("Default task - does nothing at the moment");
+gulp.task('clean', function() {
+  	del(['dist', 'src/js/app*.js']);
+});
+
+gulp.task("build", ['concatScripts'], function() {
+  	return gulp.src(["src/styles/style.css", "src/js/app.js", 'src/index.html'], { base: './src/'})
+               .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['clean'], function() {
+	gulp.start('build');
 })
