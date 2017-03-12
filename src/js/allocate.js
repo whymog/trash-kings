96d4591a40fd349482d8a383ca3dea3c57ca3455
Nick Cummings <nickmcummings@gmail.com>
2017-03-11 17:41:34 -0800

    Refactored allocate.js

--- src/js/allocate.js
+++ src/js/allocate.js
@@ -1,48 +1,42 @@
-var allocate = function(e) {
-	var assignment = e.target.dataset.assignment,
-		action = e.target.classList[0];
+const allocate = e => {
+	const assignment = e.target.dataset.assignment;
+	const action = e.target.classList[0];
 
 	if (action === "decrementAssignees") {
 		// First, check to make sure there's at least one assignee
 		if (getAssignments(assignment).length < 1) {
-			var errorMsg = new Message( { message: "Unable to decrement - no raccoons currently assigned" } );
-			return errorMsg;
+			return new Message( { message: "Unable to decrement - no raccoons currently assigned" } );
 		}
 
-		for (var i = 0; i < raccoons.length; i++) {
+		for (let i = 0; i < raccoons.length; i++) {
 			if (raccoons[i].assignment === assignment) {
 				raccoons[i].assignment = "unassigned";
 				updateAllocationLabels(assignment);
 				return;
 			}
-		}
+		};
 	} else if (action === "incrementAssignees") {
 		// First, check to make sure there's at least one unassigned raccoon
 		if (getAssignments("unassigned").length <= getChildRaccoons().length) {
-			var errorMsg = new Message( {message: "Unable to allocate - all racccoons are currently assigned" } );
-			return errorMsg;
+			return new Message( {message: "Unable to allocate - all racccoons are currently assigned" } );
 		}
 		// Next, find the first unassigned raccoon and assign to this task
-		for (var i = 0; i < raccoons.length; i++) {
-			if (raccoons[i].assignment === "unassigned" && getAge(raccoons[i]) > new Date("January 1, 1971")) {
+		for (let i = 0; i < raccoons.length; i++) {
+			if (raccoons[i].assignment === "unassigned" && isAdult(raccoons[i])) {
 				raccoons[i].assignment = assignment;
 				updateAllocationLabels(assignment);
 				return;
 			}
-		}
+		};
 	}
-
-	console.log(e);
-	console.log(e.target.dataset.assignment);
-	console.log(e.target.classList[0]);
 }
 
-var updateAllocationLabels = function(assignment) {
+const updateAllocationLabels = assignment => {
 	// Finally, update the appropriate label span so we don't have
 	// to check every time tick() fires (that was a bad and
 	// unsustainable method anyway)
 	if (assignment !== "unassigned") {
-		$('span[data-assignment=' + assignment + ']').html(getAssignments(assignment).length);
+		$(`span[data-assignment=${assignment}]`).html(getAssignments(assignment).length);
 	}
-	$('.numUnassigned').html(getAssignments("unassigned").length + " raccoons are unassigned");
+	$('.numUnassigned').html(`${getAssignments("unassigned").length} raccoons are unassigned`);
 }
